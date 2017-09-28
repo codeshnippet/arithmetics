9c33338ddb5e2150dddea5999c878f50957be8fb
Vitalii Kravets <vitalii.kravets@gmail.com>
2017-09-28 23:26:38 +0200

    Add javadoc

--- src/main/java/com/mantoring/arithmetic/services/ArithmeticService.java
+++ src/main/java/com/mantoring/arithmetic/services/ArithmeticService.java
@@ -6,10 +6,20 @@ import java.util.List;
 
 public class ArithmeticService {
 
+    /**
+     * Executes single binary operation.
+     * @param operation
+     * @return double value
+     */
     public double calculate(BinaryOperation operation){
         return 0.0;
     }
 
+    /**
+     * Executes multiple operations
+     * @param operations
+     * @return List of ordered double results
+     */
     public List<Double> calculate(BinaryOperation... operations){
         return null;
     }
